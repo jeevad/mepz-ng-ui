471cc51e56b3b7bacf8e8ef32205f9af7c612f05
Jeeva <jeevanantham.d@janaagraha.org>
2023-04-24 19:34:58 +0530

    Sidebar collapse issue fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,66 +1,68 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import 'metismenu';
 
 // declare var $: any;
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
 	isMenuOpened: boolean = false;
-	
-  constructor () { }
 
-  ngOnInit(): void {
-    $(function() {
-		$("#menu").metisMenu();
-	})
-	$(() =>{
-    // $('#menu').metisMenu();
-    $(".mobile-toggle-menu").on("click", function () {
-      $(".wrapper").addClass("toggled");
-    });
+	constructor() { }
 
-    // toggle menu button
-	$(".toggle-icon").on("click", function () {
-		if ($(".wrapper").hasClass("toggled")) {
+	ngOnInit(): void {
+		$(function () {
+			$("#menu").metisMenu();
+		})
+		$(() => {
+			// $('#menu').metisMenu();
+			$(".mobile-toggle-menu").on("click", function () {
+				$(".wrapper").addClass("toggled");
+			});
 
-			$(".wrapper").removeClass("toggled");
-			// $(".wrapper").removeClass("sidebar-hovered");
-			$(".sidebar-wrapper").off("hover");
-    	    $(".page-wrapper").addClass("changes");
+			// toggle menu button
+			$(".toggle-icon").on("click", function () {
+				if ($(".wrapper").hasClass("toggled")) {
 
-		} else {
-			$(".wrapper").addClass("toggled");
-			$(".sidebar-wrapper").unbind("hover");
-			$(".page-wrapper").removeClass("changes");
-			$(".sidebar-wrapper").hover(function () {
-				$(".wrapper").addClass("sidebar-hovered");
-				$(".wrapper").removeClass("toggled");
-			}, function () {
-				$(".wrapper").removeClass("sidebar-hovered");
-			    $(".wrapper").addClass("toggled");
-			})
-		}
-	});
-	$(".metismenu li .dropdown li a").on("click", () => {
-		$(".metismenu li ul li a").parents(".dropdown").addClass("mm-show");
-	});
-	$(".metismenu li ul .dropdown-item a").on("click", () => {
-		$(".metismenu li ul .dropdown-item a").parents("ul").addClass("mm-show");
-	});
+					$(".wrapper").removeClass("toggled");
+					// $(".wrapper").removeClass("sidebar-hovered");
+					$(".sidebar-wrapper").off("hover");
+					$(".page-wrapper").addClass("changes");
 
-	
+				} else {
+					$(".wrapper").addClass("toggled");
+					$(".sidebar-wrapper").unbind("hover");
+					$(".page-wrapper").removeClass("changes");
+					$(".sidebar-wrapper").hover(function () {
+						$(".wrapper").addClass("sidebar-hovered");
+						$(".wrapper").removeClass("toggled");
+						$(".main-content").addClass("body-width");
+					}, function () {
+						$(".wrapper").removeClass("sidebar-hovered");
+						$(".wrapper").addClass("toggled");
+						$(".main-content").removeClass("body-width");
+					})
+				}
+			});
+			$(".metismenu li .dropdown li a").on("click", () => {
+				$(".metismenu li ul li a").parents(".dropdown").addClass("mm-show");
+			});
+			$(".metismenu li ul .dropdown-item a").on("click", () => {
+				$(".metismenu li ul .dropdown-item a").parents("ul").addClass("mm-show");
+			});
 
-})
-	
-  }
+
+
+		})
+
+	}
 
 
 }
-  
+
 
 
