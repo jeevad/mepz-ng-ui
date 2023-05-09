90c75136c9c41e4e1382076b20a711106accf86f
kalaivanimaxwell <kalaivani@maxwellglobalsoftware.com>
2023-05-09 09:23:15 +0530

    Report type added

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reports',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
-
+ 
 
 }
