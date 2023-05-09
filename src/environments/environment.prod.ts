b9d9ab6385bb4a1b022011a19b00a196684bbe2f
Jeeva <jeevanantham.d@janaagraha.org>
2023-05-09 14:10:15 +0530

    env files added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://13.232.11.217/api1',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
-
-  apiUrl: 'http://localhost:3001'
-
-
-
+  apiUrl: 'http://localhost:3001',
 };
