45b58c96bfb2c85a661d10b8cd1b3c5ddd042709
Jeeva <jeevanantham.d@janaagraha.org>
2023-12-09 12:35:53 +0530

    Detail content added

--- src/app/pages/project/eqp-detail-modal/eqp-detail-modal.component.ts
+++ src/app/pages/project/eqp-detail-modal/eqp-detail-modal.component.ts
@@ -1,23 +1,27 @@
 import { Component, Input, TemplateRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ModalDismissReasons, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  ModalDismissReasons,
+  NgbActiveModal,
+  NgbModal,
+  NgbModule,
+  NgbNavModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-eqp-detail-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbNavModule, NgbModule,],
   templateUrl: './eqp-detail-modal.component.html',
   styleUrls: ['./eqp-detail-modal.component.css'],
 })
 export class EqpDetailModalComponent {
-
   @Input() eqp: any;
   private modalService = inject(NgbModal);
   closeResult = '';
+  activeTab = 1;
 
-  constructor(
-     public activeModal: NgbActiveModal,
-  ) {}
+  constructor(public activeModal: NgbActiveModal) {}
 
   open(content: TemplateRef<any>) {
     this.modalService
